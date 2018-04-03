15cb5ff5ee4101f59353100c8dcbb35ef9c22b13
GotUp <kku84@daum.net>
2018-04-03 22:24:54 +0900

    1st Commit for BeLive with UI Design.

---
+++ beLive/app/src/androidTest/java/com/samsung/hackerton18/teamr/belive/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.samsung.hackerton18.teamr.belive
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.samsung.hackerton18.teamr.belive", appContext.packageName)
+    }
+}
---
+++ beLive/app/src/main/java/com/samsung/hackerton18/teamr/belive/fragment/smartContract/TTS_ContractFragment.kt
@@ -0,0 +1,26 @@
+package com.samsung.hackerton18.teamr.belive.fragment.smartContract
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+//import android.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.samsung.hackerton18.teamr.belive.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TTS_ContractFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tts_contract, container, false)
+    }
+
+}// Required empty public constructor
